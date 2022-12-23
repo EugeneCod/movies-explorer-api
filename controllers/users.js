45b14b955035fd2d93e29e3aadddfe08bdbbe32d
Eugene Korobkin <eugene_korobkin@mail.ru>
2022-12-23 16:18:03 +0300

    fix: token

--- controllers/users.js
+++ controllers/users.js
@@ -48,7 +48,8 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: false,
+        sameSite: NODE_ENV === 'production' ? 'lax' : 'none',
+        secure: NODE_ENV === 'production',
       })
         .send({ message: RESPONSE_MESSAGES.AUTHORIZATION_WAS_SUCCESSFUL });
     })
